6f6f882c0700426627ec510c72d0671f7e0cbffb
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2025-03-03 20:25:47 +0900

    add swerve module and subsystem

--- src/main/java/frc/robot/subsystems/DriveTrain/DriveModule.java
+++ src/main/java/frc/robot/subsystems/DriveTrain/DriveModule.java
@@ -1,23 +1,51 @@
 package frc.robot.subsystems.DriveTrain;
 
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.spark.SparkLowLevel;
-import frc.robot.utils.enums.SwerveDrive.SwerveModuleCANCoderPort;
-import frc.robot.utils.enums.SwerveDrive.SwerveModulePosition;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.units.AngleUnit;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Angle;
+import frc.robot.Constants;
+import frc.robot.utils.enums.SwerveDrive.SwervePosition;
 import com.revrobotics.spark.SparkMax;
-import frc.robot.utils.enums.SwerveDrive.SwerveModuleRunningMotorPort;
-import frc.robot.utils.enums.SwerveDrive.SwerveModuleSteeringMotorPort;
 
+//スワーブの車輪一個用のクラス
 public class DriveModule {
 
+    //機器類
     private final SparkMax driveMotor;
+    private final SparkMax steeringMotor;
+    private final CANcoder encoder;
 
-    public DriveModule(SwerveModulePosition pos,
-                       SwerveModuleRunningMotorPort driveMotorPort,
-                       SwerveModuleSteeringMotorPort steeringMotorPort,
-                       SwerveModuleCANCoderPort encoderPort
-                       ){
+    private SwerveModuleState currentState;
+
+    //初期化
+    public DriveModule(SwervePosition pos,
+                       int driveMotorPort,
+                       int steeringMotorPort,
+                       int encoderPort
+    ) {
 
         this.driveMotor = new SparkMax(driveMotorPort, SparkLowLevel.MotorType.kBrushless);
+        this.steeringMotor = new SparkMax(steeringMotorPort, SparkLowLevel.MotorType.kBrushless);
+        this.encoder = new CANcoder(encoderPort);
+    }
 
+    public SwerveModulePosition getPosition() {
+        return new SwerveModulePosition(
+            driveMotor.getAbsoluteEncoder().getPosition() / Constants.SwerveConstants.gearRatio
+            * Constants.SwerveConstants.wheelRaduis * 2 * Math.PI/* add gear ratio calc */,
+            Rotation2d.fromRotations(encoder.getAbsolutePosition().getValue().in(Units.Rotations))
+            );
+    }
+
+
+    public void setState(SwerveModuleState state){
+        state.optimize(currentState.angle);
+        driveMotor.set(state.speedMetersPerSecond);
+        //driveMotor.set(state.angle);
     }
 }
--- src/main/java/frc/robot/subsystems/DriveTrain/SwerveDriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrain/SwerveDriveTrainSubsystem.java
@@ -1,42 +1,152 @@
-package frc.robot.subsystems;
+package frc.robot.subsystems.DriveTrain;
 
-
+import com.studica.frc.AHRS;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.kinematics.*;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants;
+import frc.robot.subsystems.NavXGyroSubsystem;
+import frc.robot.utils.enums.SwerveDrive.SwervePosition;
 
+
+//スワーブ全体を管理するクラス
 public class SwerveDriveTrainSubsystem extends SubsystemBase {
 
-    // With eager singleton initialization, any static variables/fields used in the 
-    // constructor must appear before the "INSTANCE" variable so that they are initialized 
-    // before the constructor is called when the "INSTANCE" variable initializes.
-
-    /**
-     * The Singleton instance of this SwerveDriveTrainSubsystem. Code should use
-     * the {@link #getInstance()} method to get the single instance (rather
-     * than trying to construct an instance of this class.)
-     */
+    //シングルトンにする
     private final static SwerveDriveTrainSubsystem INSTANCE = new SwerveDriveTrainSubsystem();
 
-    /**
-     * Returns the Singleton instance of this SwerveDriveTrainSubsystem. This static method
-     * should be used, rather than the constructor, to get the single instance
-     * of this class. For example: {@code SwerveDriveTrainSubsystem.getInstance();}
-     */
     @SuppressWarnings("WeakerAccess")
     public static SwerveDriveTrainSubsystem getInstance() {
         return INSTANCE;
     }
 
-    /**
-     * Creates a new instance of this SwerveDriveTrainSubsystem. This constructor
-     * is private since this class is a Singleton. Code should use
-     * the {@link #getInstance()} method to get the singleton instance.
-     */
-    private SwerveDriveTrainSubsystem() {
-        // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
-        //       in the constructor or in the robot coordination class, such as RobotContainer.
-        //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
-        //       such as SpeedControllers, Encoders, DigitalInputs, etc.
+    private final XboxController controller;
+    private boolean inputLock = false;
+    private final AHRS gyro = NavXGyroSubsystem.getInstance().getSensor();
+    private final SlewRateLimiter inputXLimiter = new SlewRateLimiter(3);
+    private final SlewRateLimiter inputYLimiter = new SlewRateLimiter(3);
+    private final SlewRateLimiter inputRotLimiter = new SlewRateLimiter(3);
+    private double movementSpeed = Constants.SwerveConstants.defaultMaxSpeed;
+    private double rotateSpeed = Constants.SwerveConstants.defaultAngularSpeed;
+    private boolean isFieldRelative = false;
 
+    private final DriveModule swerveFR = new DriveModule(
+        SwervePosition.FRONT_RIGHT,
+        Constants.DriveMotorPort.FRONT_RIGHT,
+        Constants.SteeringMotorPort.FRONT_RIGHT,
+        Constants.CANCoderPort.FRONT_RIGHT
+    );
+    private final DriveModule swerveFL = new DriveModule(
+        SwervePosition.FRONT_LEFT,
+        Constants.DriveMotorPort.FRONT_LEFT,
+        Constants.SteeringMotorPort.FRONT_LEFT,
+        Constants.CANCoderPort.FRONT_LEFT
+    );
+    private final DriveModule swerveRL = new DriveModule(
+        SwervePosition.REAR_LEFT,
+        Constants.DriveMotorPort.REAR_LEFT,
+        Constants.SteeringMotorPort.REAR_LEFT,
+        Constants.CANCoderPort.REAR_LEFT
+    );
+    private final DriveModule swerveRR = new DriveModule(
+        SwervePosition.REAR_RIGHT,
+        Constants.DriveMotorPort.REAR_RIGHT,
+        Constants.SteeringMotorPort.REAR_RIGHT,
+        Constants.CANCoderPort.REAR_RIGHT
+    );
+
+    private final SwerveDriveKinematics kinematics =
+        new SwerveDriveKinematics(
+            Constants.moduleLocations.FL.getValue(),
+            Constants.moduleLocations.FR.getValue(),
+            Constants.moduleLocations.RL.getValue(),
+            Constants.moduleLocations.RR.getValue()
+        );
+
+    private final SwerveDriveOdometry odometry =
+        new SwerveDriveOdometry(
+            kinematics,
+            gyro.getRotation2d(),
+            new SwerveModulePosition[]{
+                swerveFL.getPosition(),
+                swerveFR.getPosition(),
+                swerveRL.getPosition(),
+                swerveRR.getPosition(),
+            }
+        );
+
+
+    //初期化
+    public SwerveDriveTrainSubsystem() {
+        this.controller = new XboxController(Constants.OperatorConstants.DRIVER_CONTROLLER_PORT);
+    }
+
+    //モジュールの現在の位置から,オドメトリを更新する.
+    private void updateOdometry() {
+        this.odometry.update(
+            gyro.getRotation2d(),
+            new SwerveModulePosition[]{
+                swerveFL.getPosition(),
+                swerveFR.getPosition(),
+                swerveRL.getPosition(),
+                swerveRR.getPosition(),
+            }
+        );
+    }
+
+    private void drive(double x, double y, double rot) {
+        //1フレームあたりの移動量を取得
+        SwerveModuleState[] states = this.kinematics.toSwerveModuleStates(
+            ChassisSpeeds.discretize(
+                this.isFieldRelative ?
+                    ChassisSpeeds.fromFieldRelativeSpeeds(x, y, rot, this.gyro.getRotation2d())
+                    : new ChassisSpeeds(x, y, rot),
+                Constants.SwerveConstants.defaultPeriod //フレーム時間(0.02s, 50Hz)で移動速度を微分する.
+            )
+        );
+        SwerveDriveKinematics.desaturateWheelSpeeds(states, this.movementSpeed);
+    }
+
+    private Command changeSpeed(boolean increase) {
+        return runOnce(()->{
+            this.movementSpeed += increase ? 0.1:-0.1;
+            this.movementSpeed = MathUtil.clamp(this.movementSpeed,0,1);
+
+        });
+    }
+    private Command changeRotateSpeed(boolean increase) {
+        return runOnce(()->{
+            this.rotateSpeed += increase ? 0.05:-0.05;
+            this.rotateSpeed = MathUtil.clamp(this.rotateSpeed,0,Constants.SwerveConstants.maxAngularSpeed);
+        });
+    }
+
+    @Override
+    public void periodic() {
+        if (inputLock) return;
+
+        double x = inputXLimiter.calculate(MathUtil.applyDeadband(controller.getLeftY(), Constants.OperatorConstants.CONTROLLER_LEFT_DEADZONE_Y));
+        double y = inputYLimiter.calculate(MathUtil.applyDeadband(controller.getLeftY(), Constants.OperatorConstants.CONTROLLER_LEFT_DEADZONE_X));
+        double rot = inputRotLimiter.calculate(MathUtil.applyDeadband(controller.getRightX(),Constants.OperatorConstants.CONTROLLER_RIGHT_DEADZONE_X));
+
+        double speedX = x * this.movementSpeed;
+        double speedY = y * this.movementSpeed;
+        double speedRot = rot * this.rotateSpeed;
+
+        updateOdometry();
+        this.drive(speedX, speedY, speedRot);
+    }
+
+    public void setControllerBindings(CommandXboxController controller){
+        //コントローラーの矢印キーで速度関連の調整ができるようにイベントを登録
+        controller.povUp().onTrue(this.changeSpeed(true));
+        controller.povDown().onTrue(this.changeSpeed(false));
+        controller.povLeft().onTrue(this.changeRotateSpeed(true));
+        controller.povRight().onTrue(this.changeRotateSpeed(false));
     }
 }
 
