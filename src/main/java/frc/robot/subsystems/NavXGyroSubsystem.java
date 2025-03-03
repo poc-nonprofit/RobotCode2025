7fc1bb54ef539da4593c997f2a6d5520cdbcacc4
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2025-03-03 20:25:35 +0900

    add gyro subsystem

--- src/main/java/frc/robot/subsystems/NavXGyroSubsystem.java
+++ src/main/java/frc/robot/subsystems/NavXGyroSubsystem.java
@@ -2,12 +2,14 @@ package frc.robot.subsystems;
 
 
 import com.studica.frc.AHRS;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class NavXGyroSubsystem extends SubsystemBase {
 
-    private AHRS ahrs = new AHRS(AHRS.NavXComType.kMXP_SPI);
-
+    //シングルトン
     private final static NavXGyroSubsystem INSTANCE = new NavXGyroSubsystem();
 
     @SuppressWarnings("WeakerAccess")
@@ -15,8 +17,28 @@ public class NavXGyroSubsystem extends SubsystemBase {
         return INSTANCE;
     }
 
-    private NavXGyroSubsystem() {
+    private final AHRS ahrs = new AHRS(AHRS.NavXComType.kMXP_SPI);
+    private final DoublePublisher publisher;
 
+    //初期化
+    private NavXGyroSubsystem() {
+        //角度をリセット
+        ahrs.zeroYaw();
+        //ダッシュボードに表示するウィジェット
+        publisher = NetworkTableInstance.getDefault()
+            .getDoubleTopic("NavX Gyro Rotation (radians)").publish();
+    }
+
+
+    public AHRS getSensor (){
+        return this.ahrs;
+    }
+
+    //
+    @Override
+    public void periodic(){
+        //角度を更新
+        publisher.set(Rotation2d.fromDegrees(ahrs.getAngle()).getRadians());
     }
 }
 
