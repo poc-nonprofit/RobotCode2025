9a0ced19377bb0d4168409f025a5c1fbd6432a4f
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2025-03-14 19:00:29 +0900

    fix multiple init and add default command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
@@ -15,33 +16,42 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 
-
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
-public class RobotContainer
-{
+public class RobotContainer {
     // The robot's subsystems and commands are defined here...
     private final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
     //スワーブを管理するサブシステムのインスタンス
-    private final SwerveDriveTrainSubsystem swerveDriveTrainSubsystem = new SwerveDriveTrainSubsystem();
-    
+    private final SwerveDriveTrainSubsystem swerveDriveTrainSubsystem = SwerveDriveTrainSubsystem.getInstance();
+
     // Replace with CommandPS4Controller or CommandJoystick if needed
     private final CommandXboxController driverController =
-            new CommandXboxController(OperatorConstants.DRIVER_CONTROLLER_PORT);
-    
-    
-    /** The container for the robot. Contains subsystems, OI devices, and commands. */
-    public RobotContainer()
-    {
+        new CommandXboxController(OperatorConstants.DRIVER_CONTROLLER_PORT);
+    private final CommandXboxController operatorController =
+        new CommandXboxController(OperatorConstants.OPERATOR_CONTROLLER_PORT);
+
+
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
         // Configure the trigger bindings
         configureBindings();
+
+        swerveDriveTrainSubsystem.setDefaultCommand(
+            new RunCommand(() -> swerveDriveTrainSubsystem.drive(
+                driverController.getLeftY(),
+                driverController.getLeftX(),
+                driverController.getRightX()
+            ),swerveDriveTrainSubsystem)
+        );
     }
-    
-    
+
+
     /**
      * Use this method to define your trigger->command mappings. Triggers can be created via the
      * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
@@ -51,26 +61,24 @@ public class RobotContainer
      * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
      * joysticks}.
      */
-    private void configureBindings()
-    {
+    private void configureBindings() {
         // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
         new Trigger(exampleSubsystem::exampleCondition)
-                .onTrue(new ExampleCommand(exampleSubsystem));
-        
+            .onTrue(new ExampleCommand(exampleSubsystem));
+
         // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
         // cancelling on release.
-        driverController.b().whileTrue(exampleSubsystem.exampleMethodCommand());
+        //driverController.b().whileTrue(exampleSubsystem.exampleMethodCommand());
         SwerveDriveTrainSubsystem.getInstance().setControllerBindings(driverController);
     }
-    
-    
+
+
     /**
      * Use this to pass the autonomous command to the main {@link Robot} class.
      *
      * @return the command to run in autonomous
      */
-    public Command getAutonomousCommand()
-    {
+    public Command getAutonomousCommand() {
         // An example command will be run in autonomous
         return Autos.exampleAuto(exampleSubsystem);
     }
