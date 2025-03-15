6753d4f1cc38c8b2b4d4acd7e2168775e8c39c7b
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2025-03-15 20:49:47 +0900

    add autonomous command

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -5,23 +5,25 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.subsystems.DriveTrain.SwerveDriveTrainSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 
-
+//Autonomous 用のコマンドクラス
 public final class Autos
 {
-    /** Example static factory for an autonomous command. */
-    public static Command exampleAuto(ExampleSubsystem subsystem)
+    //コマンドを作成して実行
+    public static Command runOnlyForwardAuto(SwerveDriveTrainSubsystem driveTrain)
     {
-        return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
-    }
-    
-    
-    private Autos()
-    {
-        throw new UnsupportedOperationException("This is a utility class!");
+        return new RunCommand(()->{
+            //ちょっとづつ前に進むだけ
+            driveTrain.drive(
+                -0.5,0,0
+            );
+        },driveTrain);
+        //return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
     }
 }
